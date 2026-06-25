4b415fbaff3f09f0ff1f6593a09cd6a4526a16aa
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-06-25 14:57:22 +0800

    feat: change to get the name of the employee and feat. can createUsers

--- app/Http/Controllers/Dashboard/SampleController.php
+++ app/Http/Controllers/Dashboard/SampleController.php
@@ -13,21 +13,25 @@ class SampleController extends Controller
         //$employees = Employee::all();
         //return $employees;
 
-        $users = Employee::where('id', 1)
-            -> get();
+        // $users = Employee::where('id', 1)
+        //     -> get();
 
-        if (count($users) > 0){
-            return $users;
-        }
-        else
-            {
-                return "No record found.";
-            }
+        // if (count($users) > 0){
+        //     return $users;
+        // }
+        // else
+        //     {
+        //         return "No record found.";
+        //     }
         // $users = Employee::find(1); objoect or single data
 
         //checking if the it contains data using single data
-        // $users = Employee::where('id', '=', 1)
-        // -> first(); // single data
+        $users = Employee::select('name')
+            -> where('id', '=', 2)
+            -> first(); // single data
+
+        // can also use this:
+        return $users ? $users: "No record Found.";
 
         // if ($users) {
         //     return $users;
@@ -35,5 +39,16 @@ class SampleController extends Controller
         // else {
         //     return "No record Found.";
         // }
+    }
+
+    public function createUser()
+    {
+        $insert = Employee::create([
+            'name' => 'Renalyn',
+            'email' => 'Renalyntrndd@gmail.com',
+            'password' => '12345',
+        ]);
+
+        return $insert ? "Success" : "Error";
     }
 }
