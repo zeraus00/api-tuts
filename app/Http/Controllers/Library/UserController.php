68f23f8595b59812c5c33bd4ff29ccf2913c990b
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-06-25 13:44:38 +0800

    refactor: remove diff. way to get users

--- app/Http/Controllers/Library/UserController.php
+++ app/Http/Controllers/Library/UserController.php
@@ -9,14 +9,8 @@ class UserController extends Controller
 {
     public function getUsers()
     {
-        // $users = Employee::all(); //get all users of Employee
-        // return $users; // return the users
-        $users = Employee::where('id', 1)
-            -> get();
-
-        // $users = Employee::find(1); objoect or single data
-        // $users = Employee::where('id', '=', 1)
-        // -> first();
-        return $users;
+        $users = Employee::all(); //get all users of Employee
+        return $users; // return the users
+        
     }
 }
