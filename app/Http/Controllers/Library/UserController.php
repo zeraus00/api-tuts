e25c8ac4805d8fd74689659b41b61825509c34ef
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-06-25 08:45:13 +0800

    add: added UserController

--- app/Http/Controllers/Library/UserController.php
+++ app/Http/Controllers/Library/UserController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Library;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Employee;
 class UserController extends Controller
 {
-    //
+    public function getUsers()
+    {
+        $users = Employee::all();
+        return $users;
+    }
 }
