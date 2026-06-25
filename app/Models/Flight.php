b439b70aa5dfd81bac882608bcbdd0426e3564e7
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-06-25 13:34:11 +0800

    feat: added flight table

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -9,4 +9,5 @@ class Flight extends Model
 {
     use HasFactory;
     protected $table = "flights"; //defines the table that will be use in the model
+    // public $timestamps = false; if table don't have 'created at'
 }
