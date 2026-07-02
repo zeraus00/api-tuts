c8a97281de401b2ec36266a337081affd13005cf
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-07-02 15:16:02 +0800

    add: new model for employee

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $with = [
+        'permissions',
+    ];
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+    
+}
